ce8bb9e38b92530dc2c20426bc04e0c55f7fe5e0
Michael Anderson <admin@somescape.org>
2013-05-23 04:15:09 -0700

    Created a bootstrapper for the new GUI that overwrites some rqt functions in order to eliminate the need to fork rqt. While running rqt using the bootstrapper, the plugin list is condensed and the window title is modified. The code injection system used for these systems is highly extensible to other modifications. Next I will need to add modifications to enable controller GUI plugins to be loaded in an elegant fashion.
    Additionally, a new stack and several new packages were created. I selected the acronym agp (Atrias GUI Plugin) to serve as a suffix for GUI plugin packages. If you have a nicer one in mind, please let me know.
    
    New packages & stacks:
    * atrias_gui_comm - - - - - - - Will house the communications backend for the GUI
    
    * atrias_gui_plugins  - - - - - New stack for atrias_gui (rqt) plugins
       * agp_controller_select  - - Plugin for selecting a robot controller
       * agp_debugging  - - - - - - Plugin for troubleshooting e-stops, etc.
       * agp_robot_hud  - - - - - - Heads Up Display for important information
       * agp_robot_status - - - - - Like current Robot Status window, but better
       * agp_robot_visualizations - Carrot and other robot rendering things
    
    On an unrelated note, Python is actually pretty impressive.

---
+++ software/atrias_gui/src/atrias_gui/magic.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from python_qt_binding.QtGui import QAction, QIcon, QMenu
+from qt_gui.menu_manager import MenuManager
+from qt_gui.plugin_menu import PluginMenu
+from qt_gui.main_window import MainWindow
+
+disabled_plugins = [
+        "rqt_web/Web",
+        "rqt_py_console/PyConsole",
+        "rqt_image_view/ImageView",
+        "rqt_action/ActionPlugin",
+        "rqt_dep/RosPackGraph",
+        "rqt_reconfigure/Param",
+        "rqt_logger_level/LoggerLevel",
+        "rqt_graph/RosGraph",
+        "rqt_service_caller/ServiceCaller",
+        "rqt_srv/Services",
+        "rqt_publisher/Publisher",
+        "rqt_msg/Messages",
+        "rqt_launch/Launch",
+        "rqt_console/Console"
+    ]
+
+"""
+Our lovely little impostor function
+"""
+def add_plugin_impostor(current_function):
+    def new_func(self, plugin_descriptor):
+        if add_plugin_additions(self, plugin_descriptor) == False:
+            return
+        current_function(self, plugin_descriptor)
+    return new_func
+
+def add_plugin_additions(self, plugin_descriptor):
+    if plugin_descriptor.plugin_id() in disabled_plugins:
+        return False
+    #print plugin_descriptor.plugin_id()
+
+"""
+Might as well change the name of the window while we're at it >:D
+"""
+def perspective_changed_impostor(current_function):
+    def new_func(self, name):
+        current_function(self, name)
+        perspective_changed_additions(self, name)
+    return new_func
+
+def perspective_changed_additions(self, name):
+    self.setWindowTitle('%s - ATRIAS Control Interface 3.0' % str(name))
+
+def magic(sfile):    
+    try:
+        import rospkg
+        r = rospkg.RosPack()
+        p = r.get_path('rqt_gui')
+        sys.path.append(os.path.join(p, 'src'))
+    except ImportError:
+        pass
+    
+    """
+    Here we hamfistedly murder the original add_plugin function.
+    We then substitute an impostor function that can make any
+    plugins that we don't like disappear.
+    """
+    PluginMenu.add_plugin = add_plugin_impostor(PluginMenu.add_plugin)
+    MainWindow.perspective_changed = perspective_changed_impostor(MainWindow.perspective_changed)
+    #from atrias_gui.main import Main
+    from rqt_gui.main import Main
+    main = Main(os.path.abspath(sfile))
+    return main.main()
